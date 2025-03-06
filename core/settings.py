b4326e17a0ec4947058109a44ed5ce7478158a72
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-03-06 13:10:47 +0500

    fixed

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["api.xorazmfc.uz"]
 
 
 # Application definition
