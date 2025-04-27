67b209b2ae3080f13fdf25deec75bcb77a677708
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-04-27 20:13:09 +0500

    fix

--- common/models.py
+++ common/models.py
@@ -32,7 +32,7 @@ class News(BaseModel):
     image = models.ImageField(upload_to='news/%Y/%m/')
     category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE, related_name='news')
     video = models.FileField(upload_to='news/%Y/%m/', null=True, blank=True)
-    link = models.URLField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.title
